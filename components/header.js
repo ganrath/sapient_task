b87f40b898ecb62cffb3be4e2cdffefcb2b1e8f5
Bharath Gan <ganrath07@gmail.com>
2020-01-07 10:58:43 +0530

    Add files via upload

---
+++ components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <img src="https://www.publicissapient.com/content/dam/ps-rebrand/brand/ps-logo-NEW.svg" />
+      </header>
+    </div>
+  );
+}
+
+export default Header;
